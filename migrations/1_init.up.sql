0e5d40221d2423a31beea78b1bb0836055497438
James Skarzinskas <james@jskarzin.org>
2021-09-09 20:29:22 -0400

    Seed data tweaks to help with skills/spells dev

--- migrations/1_init.up.sql
+++ migrations/1_init.up.sql
@@ -299,7 +299,7 @@ VALUES
 INSERT INTO
     player_characters(id, username, password_hash, wizard, room_id, race_id, job_id, level, experience, practices, health, max_health, mana, max_mana, stamina, max_stamina, stat_str, stat_dex, stat_int, stat_wis, stat_con, stat_cha, stat_lck)
 VALUES
-    (1, 'Admin', '$2a$10$sS5pzrKaD9qeG3ntkT7.gOohefnxSy/9OHR/p1uImyTL2edzYeJzW', 1, 1, 1, 1, 60, 0, 0, 100, 100, 100, 100, 100, 100, 18, 18, 18, 18, 18, 18, 18);
+    (1, 'Admin', '$2a$10$sS5pzrKaD9qeG3ntkT7.gOohefnxSy/9OHR/p1uImyTL2edzYeJzW', 1, 1, 1, 3, 60, 0, 0, 100, 100, 100, 100, 100, 100, 18, 18, 18, 18, 18, 18, 18);
 
 /* Test NPC in Limbo area */
 INSERT INTO
--- migrations/2_skills.up.sql
+++ migrations/2_skills.up.sql
@@ -38,6 +38,9 @@ CREATE TABLE pc_skill_proficiency (
 INSERT INTO skills(id, name, type) VALUES (1, 'dodge', 'passive');
 INSERT INTO skills(id, name, type) VALUES (2, 'unarmed combat', 'passive');
 INSERT INTO skills(id, name, type) VALUES (3, 'peek', 'passive');
+INSERT INTO skills(id, name, type) VALUES (4, 'armor', 'spell');
+INSERT INTO skills(id, name, type) VALUES (5, 'fireball', 'spell');
+INSERT INTO skills(id, name, type) VALUES (6, 'bash', 'skill');
 
 /* Grant unarmed combat as a seed skill for all four base jobs with varying complexity and cost */
 INSERT INTO job_skill(id, job_id, skill_id, level, complexity, cost) VALUES (1, 1, 2, 1, 1, 1);
@@ -45,7 +48,24 @@ INSERT INTO job_skill(id, job_id, skill_id, level, complexity, cost) VALUES (2, 
 INSERT INTO job_skill(id, job_id, skill_id, level, complexity, cost) VALUES (3, 1, 2, 3, 5, 5);
 INSERT INTO job_skill(id, job_id, skill_id, level, complexity, cost) VALUES (4, 1, 2, 4, 5, 5);
 
-/* Grant it mastered to the seed admin user as well */
+/* Warrior defaults: bash */
+INSERT INTO job_skill(id, job_id, skill_id, level, complexity, cost) VALUES (5, 1, 6, 1, 5, 20);
+
+/* Thief defaults: peek */
+INSERT INTO job_skill(id, job_id, skill_id, level, complexity, cost) VALUES (6, 2, 3, 1, 5, 5);
+
+/* Cleric defaults: armor */
+INSERT INTO job_skill(id, job_id, skill_id, level, complexity, cost) VALUES (7, 4, 4, 1, 1, 20);
+
+/* Mage defaults: fireball */
+INSERT INTO job_skill(id, job_id, skill_id, level, complexity, cost) VALUES (8, 3, 5, 1, 1, 20);
+
+/* Grant some skills mastered to the seed admin user as well */
 INSERT INTO pc_skill_proficiency(id, player_character_id, skill_id, proficiency) VALUES (1, 1, 2, 100);
+INSERT INTO pc_skill_proficiency(id, player_character_id, skill_id, proficiency) VALUES (2, 1, 2, 100);
+INSERT INTO pc_skill_proficiency(id, player_character_id, skill_id, proficiency) VALUES (3, 1, 3, 100);
+INSERT INTO pc_skill_proficiency(id, player_character_id, skill_id, proficiency) VALUES (4, 1, 4, 100);
+INSERT INTO pc_skill_proficiency(id, player_character_id, skill_id, proficiency) VALUES (5, 1, 5, 100);
+INSERT INTO pc_skill_proficiency(id, player_character_id, skill_id, proficiency) VALUES (6, 1, 6, 100);
 
 CREATE INDEX index_skill_name ON skills(name);
