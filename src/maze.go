6df059c3ec32371044ab4e096d3a13ce27732ffa
James Skarzinskas <james@jskarzin.org>
2021-09-10 18:38:31 -0400

    Add virtual flag to Room struct

---
+++ src/maze.go
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021 James Skarzinskas.
+ * All rights reserved.
+ * See LICENSE.txt in project root for license information.
+ * Authors:
+ *     James Skarzinskas <james@jskarzin.org>
+ */
+package main
+
+type MazeGrid struct {
+}
+
+type MazeCell struct {
+}
+
+/* Generate rooms in a maze using Prim's algorithm */
+func GenerateMaze(width int, height int) {
+	
+}
