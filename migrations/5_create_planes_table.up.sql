fc9d72dc6b947c226c2265ee9d0360ba2fc6b032
James Skarzinskas <james@jskarzin.org>
2021-10-08 12:48:22 -0400

    Add planes.go, add optional zone_id reference on planes

--- migrations/5_create_planes_table.up.sql
+++ migrations/5_create_planes_table.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE planes (
     `id` BIGINT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL UNIQUE,
+    `zone_id` INT,
 
     `plane_type` ENUM('void', 'maze', 'wilderness') NOT NULL DEFAULT 'void',
     `source_type` ENUM('void', 'blob') NOT NULL DEFAULT 'void',
@@ -13,7 +14,8 @@ CREATE TABLE planes (
     `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
 
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (zone_id) REFERENCES zones(id),
 );
 
 INSERT INTO planes(id, name, plane_type, source_type, width, height) VALUES (1, 'limbo', 'void', 50, 50);
---
+++ src/plane.go
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2021 James Skarzinskas.
+ * All rights reserved.
+ * See LICENSE.txt in project root for license information.
+ * Authors:
+ *     James Skarzinskas <james@jskarzin.org>
+ */
