8aaec4d836a5883524f6fc23ff52635279467e87
James Skarzinskas <james@jskarzin.org>
2021-10-17 22:20:03 -0400

    Add migrations for connections between scripts and rooms, mobiles, and objects

--- migrations/6_create_scripts_table.up.sql
+++ migrations/6_create_scripts_table.up.sql
@@ -10,4 +10,46 @@ CREATE TABLE scripts (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE mobile_script (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `mobile_id` BIGINT NOT NULL,
+    `script_id` BIGINT NOT NULL,
+
+    /* Timestamps */
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (mobile_id) REFERENCES mobiles(id),
+    FOREIGN KEY (script_id) REFERENCES scripts(id)
+);
+
+CREATE TABLE object_script (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `object_id` BIGINT NOT NULL,
+    `script_id` BIGINT NOT NULL,
+
+    /* Timestamps */
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (`object_id`) REFERENCES objects(id),
+    FOREIGN KEY (script_id) REFERENCES scripts(id)
+);
+
+CREATE TABLE room_script (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `room_id` BIGINT NOT NULL,
+    `script_id` BIGINT NOT NULL,
+
+    /* Timestamps */
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (room_id) REFERENCES rooms(id),
+    FOREIGN KEY (script_id) REFERENCES scripts(id)
+);
+
 CREATE INDEX index_script_name ON scripts(name);
