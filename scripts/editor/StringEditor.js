a6ec9c63b2980659eaf07d69e40a6fd74b61dbd9
James Skarzinskas <james@jskarzin.org>
2021-12-26 10:52:32 -0500

    Simplify string editor line-splitting method and fix for empty lines

--- scripts/editor/StringEditor.js
+++ scripts/editor/StringEditor.js
@@ -7,21 +7,20 @@
  */
 Golem.StringEditor = function (client, string, callback) {
     let _string = String(string);
-    let _initialLineMatches = string.match(/[^\r\n]+/g);
+    let _initialLineMatches = string.replace("\r\n", "\n").split(/\n/g);
     let cursor = _initialLineMatches ? _initialLineMatches.length : 0;
 
     const commit = () => (string = String(_string));
     const ch = client.character;
-
-    function showEditorBuffer() {
-        const lines = _string.match(/[^\r\n]+/g);
+    const showEditorBuffer = () => {
+        const lines = _string.replace("\r\n", "\n").split(/\n/g);;
 
         if(lines) {
             for(let i = 0; i < lines.length; i++) {
                 ch.send((i + 1) + ". " + lines[i] + "\r\n");
             }
         }
-    }
+    };
 
     ch.send(
         "\r\n{Y! PRINT | @ DISCARD | $ WRITE | :# GOTO | :d# DELETE | :i# NEW LINES\r\n" +
@@ -37,7 +36,7 @@ Golem.StringEditor = function (client, string, callback) {
             }
 
             if(!['!', '@', '$', '?'].includes(input) && input[0] !== ':') {
-                const lines = _string.match(/[^\r\n]+/g);
+                const lines = _string.replace("\r\n", "\n").split(/\n/g);
 
                 if(lines && lines.length + 1 > Golem.StringEditor.MaxAllowedLines) {
                     ch.send("{RString buffer is full, delete lines to make some room!{x\r\n");
@@ -71,12 +70,8 @@ Golem.StringEditor = function (client, string, callback) {
                 let [_, lineMatch] = Golem.StringEditor.LineNavigationRegex.exec(input);
                 let m = null;
                 let lineNumber = parseInt(lineMatch);
-                
-                const lines = [];
 
-                while (null !== (m = Golem.StringEditor.LinesRegex.exec(_string))) {
-                    lines.push(m.index);
-                }
+                const lines = _string.replace("\r\n", "\n").split(/\n/g);
 
                 if(lineNumber > lines.length) {
                     ch.send("{R! Cannot move out of bounds (" + lines.length + " lines in buffer){x\r\n");
@@ -87,7 +82,7 @@ Golem.StringEditor = function (client, string, callback) {
                 cursor = lineNumber;
             } else if(Golem.StringEditor.DeleteLineRegex.test(input)) {
                 const [_, lineMatch] = Golem.StringEditor.DeleteLineRegex.exec(input),
-                    lines = _string.match(/[^\r\n]+/g);
+                    lines = _string.replace("\r\n", "\n").split(/\n/g);
                 const lineNumber = parseInt(lineMatch);
 
                 if(lineNumber > Golem.StringEditor.MaxAllowedLines) return;
@@ -101,27 +96,28 @@ Golem.StringEditor = function (client, string, callback) {
                         .join("\r\n");
                 }
 
-                ch.send("{WOk.  Deleted " + lineNumber + " lines from line " + lineNumber + "{x\r\n");
+                ch.send("{WOk.  Deleted " + lineNumber + " lines from line " + cursor + "{x\r\n");
             } else if(Golem.StringEditor.InsertLineRegex.test(input)) {
                 const [_, lineMatch] = Golem.StringEditor.InsertLineRegex.exec(input),
-                    lines = _string.match(/[^\r\n]+/g);
+                    lines = _string.replace("\r\n", "\n").split(/\n/g);
                 const lineNumber = parseInt(lineMatch);
 
                 if(lines) {
                     if(lines.length + lineNumber > Golem.StringEditor.MaxAllowedLines) return;
 
                     // lines in buffer prior to the cursor
-                    _string = lines.filter((_, index) => index < cursor)
+                    _string = lines
+                        .filter((_, index) => index < cursor)
                         // insert lineNumber empty lines
                         .concat(
-                            [...Array(lineNumber)].map(_ => ("\r\n")))
+                            [...Array(lineNumber)].map(_ => ""))
                         // remainder of original string
                         .concat(lines.filter(
                             (_, index) => (index >= cursor)))
                         // stringified
                         .join("\r\n");
                 } else {
-                    _string = "\r\n".repeat(lineNumber);
+                    _string = String("\r\n".repeat(lineNumber));
                 }
 
                 ch.send("{WOk.  Inserted " + lineNumber + " empty lines at line " + cursor + "{x\r\n");
@@ -134,7 +130,6 @@ Golem.StringEditor = function (client, string, callback) {
 
 Golem.StringEditor.MaxAllowedLines = 256;
 Golem.StringEditor.MaxLineLength = 256;
-Golem.StringEditor.LinesRegex = new RegExp(/[^\r\n]+/igm);
 Golem.StringEditor.LineNavigationRegex = new RegExp(/^\:(\d+)/);
 Golem.StringEditor.InsertLineRegex = new RegExp(/^\:i(\d+)/);
 Golem.StringEditor.DeleteLineRegex = new RegExp(/^\:d(\d+)/);
