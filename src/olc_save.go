01844ffa4aca5d4641b63e171d48d8c178914333
James Skarzinskas <james@jskarzin.org>
2022-01-02 11:25:02 -0500

    Add Character.Sync and ObjectInstance.Sync methods to UPDATE instance properties on the parent index record

--- src/olc_save.go
+++ src/olc_save.go
@@ -34,6 +34,103 @@ func (room *Room) Save() error {
 	return nil
 }
 
+// Updates an object's parent index with this instance's properties
+func (obj *ObjectInstance) Sync() error {
+	if obj.ParentId == 0 {
+		return errors.New("trying to sync an object instance with no parent ID")
+	}
+
+	_, err := obj.Game.db.Exec(`
+		UPDATE
+			objects
+		SET
+			name = ?,
+			short_description = ?,
+			long_description = ?,
+			description = ?,
+			flags = ?,
+			item_type = ?,
+			value_1 = ?,
+			value_2 = ?,
+			value_3 = ?,
+			value_4 = ?
+		WHERE
+			id = ?
+	`, obj.Name, obj.ShortDescription, obj.LongDescription, obj.Description, obj.Flags, obj.ItemType, obj.Value0, obj.Value1, obj.Value2, obj.Value3, obj.ParentId)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Updates a mob instance's index with this instance's properties
+func (ch *Character) Sync() error {
+	if ch.Flags&CHAR_IS_PLAYER != 0 || ch.Id == 0 {
+		return errors.New("invalid NPC tried to sync")
+	}
+
+	_, err := ch.Game.db.Exec(`
+		UPDATE
+			mobiles
+		SET
+			name = ?,
+			short_description = ?,
+			long_description = ?,
+			description = ?,
+			race_id = ?,
+			job_id = ?,
+			flags = ?,
+			level = ?,
+			gold = ?,
+			experience = ?,
+			health = ?,
+			max_health = ?,
+			mana = ?,
+			max_mana = ?,
+			stamina = ?,
+			max_stamina = ?,
+			stat_str = ?,
+			stat_dex = ?,
+			stat_int = ?,
+			stat_wis = ?,
+			stat_con = ?,
+			stat_cha = ?,
+			stat_lck = ?
+		WHERE
+			id = ?
+	`,
+		ch.Name,
+		ch.ShortDescription,
+		ch.LongDescription,
+		ch.Description,
+		ch.Race.Id,
+		ch.Job.Id,
+		ch.Flags,
+		ch.Level,
+		ch.Gold,
+		ch.Experience,
+		ch.Health,
+		ch.MaxHealth,
+		ch.Mana,
+		ch.MaxMana,
+		ch.Stamina,
+		ch.MaxStamina,
+		ch.Stats[STAT_STRENGTH],
+		ch.Stats[STAT_DEXTERITY],
+		ch.Stats[STAT_INTELLIGENCE],
+		ch.Stats[STAT_WISDOM],
+		ch.Stats[STAT_CONSTITUTION],
+		ch.Stats[STAT_CHARISMA],
+		ch.Stats[STAT_LUCK],
+		ch.Id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (exit *Exit) Delete() error {
 	_, err := exit.Room.Game.db.Exec(`
 		DELETE FROM
