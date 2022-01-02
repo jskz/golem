82208337721225021ed12888280ee076b2be058e
James Skarzinskas <james@jskarzin.org>
2022-01-02 07:50:46 -0500

    Add Exit.Finalize, Exit.Save, Exit.Delete methods for managing permanent exit records

--- src/olc_save.go
+++ src/olc_save.go
@@ -7,7 +7,10 @@
  */
 package main
 
-import "errors"
+import (
+	"errors"
+	"log"
+)
 
 func (room *Room) Save() error {
 	if room.Flags&ROOM_VIRTUAL != 0 || room.Flags&ROOM_PLANAR != 0 {
@@ -31,6 +34,66 @@ func (room *Room) Save() error {
 	return nil
 }
 
+func (exit *Exit) Delete() error {
+	_, err := exit.Room.Game.db.Exec(`
+		DELETE FROM
+			exits
+		WHERE
+			id = ?
+	`, exit.Id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (exit *Exit) Finalize() error {
+	if exit.Id != 0 {
+		return errors.New("trying to finalize an exit when already finalized")
+	}
+
+	if exit.Room.Id == 0 || exit.To.Id == 0 {
+		return errors.New("currently unsupported saving exit between virtual rooms")
+	}
+
+	result, err := exit.Room.Game.db.Exec(`
+		INSERT INTO
+			exits(room_id, to_room_id, direction, flags)
+		VALUES
+			(?, ?, ?, ?)
+	`, exit.Room.Id, exit.To.Id, exit.Direction, exit.Flags)
+	if err != nil {
+		log.Printf("Failed to finalize exit: %v.\r\n", err)
+		return err
+	}
+
+	exitId, err := result.LastInsertId()
+	if err != nil {
+		log.Printf("Failed to retrieve insert id: %v.\r\n", err)
+		return err
+	}
+
+	exit.Id = uint(exitId)
+	return nil
+}
+
 func (exit *Exit) Save() error {
+	if exit.Id == 0 {
+		return errors.New("trying to update an exit before it was finalized")
+	}
+
+	_, err := exit.Room.Game.db.Exec(`
+		UPDATE
+			exits
+		SET
+			flags = ?
+		WHERE
+			id = ?
+	`, exit.Flags, exit.Id)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
