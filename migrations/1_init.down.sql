7a2f73e11b7ebfe830a134b293856f09a83aeb8c
James Skarzinskas <james@jskarzin.org>
2026-05-29 08:18:31 +0300

    fix: reordered DROP statements to avoid breaking attempts to drop FK tables before parents, added missing object_instances DROP

--- migrations/1_init.down.sql
+++ migrations/1_init.down.sql
@@ -3,12 +3,13 @@ DROP INDEX index_race_name ON races;
 DROP INDEX index_job_name ON jobs;
 
 DROP TABLE exits;
-DROP TABLE rooms;
 DROP TABLE player_character_object;
-DROP TABLE objects;
-DROP TABLE jobs;
-DROP TABLE races;
 DROP TABLE player_characters;
 DROP TABLE mobiles;
 DROP TABLE resets;
+DROP TABLE rooms;
+DROP TABLE objects;
+DROP TABLE object_instances;
+DROP TABLE jobs;
+DROP TABLE races;
 DROP TABLE zones;
--- migrations/6_create_scripts_table.down.sql
+++ migrations/6_create_scripts_table.down.sql
@@ -3,4 +3,5 @@ DROP INDEX index_script_name ON scripts;
 DROP TABLE mobile_script;
 DROP TABLE room_script;
 DROP TABLE object_script;
+DROP TABLE plane_script;
 DROP TABLE scripts;
